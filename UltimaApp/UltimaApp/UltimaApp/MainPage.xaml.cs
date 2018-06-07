f7e55341c63b0aa2aeaf84d831c6a860044eb694
pipebfmv <heavypipe18@gmail.com>
2018-06-07 18:08:44 -0500

    parte uno

--- UltimaApp/UltimaApp/UltimaApp/MainPage.xaml.cs
+++ UltimaApp/UltimaApp/UltimaApp/MainPage.xaml.cs
@@ -1,42 +1,64 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using UltimaApp.Models;
 using Xamarin.Forms;
 
 namespace UltimaApp
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "https://jonmidpruebanode.herokuapp.com/users";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<Users> _user;
+        String c;
 
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        
+        
         // Se crea el metodo que se llama cuando se da click al boton
         async private void ButtonClicked(object sender, EventArgs e)
         {
-            // Vamos a validar que los campos no esten vacios
+            
+      
+                     // Vamos a validar que los campos no esten vacios
             if (string.IsNullOrEmpty(entryUser.Text) || string.IsNullOrEmpty(entryPasword.Text))
 
             {
 
                 await DisplayAlert("Error", "Debe escribir usuario y contraseña", "OK");
             }
-            else
+            else if (entryUser.Text=="pipe")
             {
-
+               // pasar();
                 await DisplayAlert("Correcto", "Bienvenido", "OK");
                 await Navigation.PushAsync(new home());
             }
+            else
+            {
+                await DisplayAlert("Error", "EL USUARIO NO EXISTE", "OK");
+            }
 
+            
         }
 
+
+    
         async private void ButtonRegistro(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Registro());
         }
 
     }
+    
 }
